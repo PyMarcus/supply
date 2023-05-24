c7c12f1588b43a20052ed5d275eb7e629809cfb3
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-05-24 00:30:08 -0300

    initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/*
+ * Programa para treinar a manipulação de arquivos em C
+ * Receber quantidade de produtos
+ * Manipular conforme os comandos passados, esta lista de produtos.
+ * */
+
+void readFile(char *fileName)
+{
+
+}
+
+
+int main(int argc, char *argv[]) {
+
+
+
+
+    return 0;
+}
