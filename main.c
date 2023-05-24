bfd08636cba608cdf4ae5abea28c6ff03cb28aa6
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-05-24 03:14:03 -0300

    xp 1

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 
 /*
@@ -7,15 +8,74 @@
  * Manipular conforme os comandos passados, esta lista de produtos.
  * */
 
-void readFile(char *fileName)
+
+struct content{
+    char text[100];
+};
+
+
+
+/*obtem a quantidade de linhas do arquivo*/
+int getLines(FILE *file)
 {
 
+    int count = 0;
+    char rows;
+
+    while(fread (&rows, sizeof(char), 1, file))
+    {
+        if(rows == '\n'){
+            count++;
+        }
+    }
+    return count;
+}
+
+
+/* função que le o arquivo determinado.*/
+int readFile(char *filePath, int row)
+{
+    char rows;
+    char convert[2];
+    int MAX_COLUMNS = 1000;
+    char store[MAX_COLUMNS];
+    int count = 0;
+    FILE *_file_ = fopen(filePath, "r");
+    FILE *file = fopen(filePath, "r");
+    int lines = getLines(_file_);
+    struct content phrases[lines];
+
+    // se o local do arquivo estiver incorreto, retorna mensagem de nao encontrado
+    if(file == NULL)
+    {
+        printf("[-]file not found!\n", stderr);
+        return 1;
+    }
+
+
+    // le arquivo:
+    while(fread (&rows, sizeof(char), 1, file)) {
+        if(rows == '\n') {
+            // copia a string formada para cada index do array
+            strcpy(phrases[count].text, store);
+            // reseta string
+            strcpy(store, "");
+            count++;
+        }
+        convert[0] = rows;
+        convert[1] = '\0';
+        // concatena os caracteres para formar a linha completa
+        strcat(store, convert);
+    }
+
+    fclose(file);
+    return 0;
 }
 
 
 int main(int argc, char *argv[]) {
 
-
+    readFile("C:\\Users\\Marcu\\CLionProjects\\supply\\input.txt", 1);
 
 
     return 0;
